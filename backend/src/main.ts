21899dd19f694c2f34d98733ca9efba60c8bf095
Lucas Castelano <lucas.castelano27@gmail.com>
2026-04-27 14:56:54 -0300

    config: prisma, prettier, environment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+import { environment } from './common/config/environment'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  const logger = new Logger('Bootstrap')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  const config = new DocumentBuilder()
+    .setTitle('Insurance Management API')
+    .setDescription('Insurance Management system API')
+    .setVersion('0.0.1')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/docs', app, document)
+
+  await app.listen(environment.PORT)
+  logger.log(`API running on http://localhost:${environment.PORT}`)
+  logger.log(`Swagger docs available at http://localhost:${environment.PORT}/docs`)
 }
-bootstrap();
+void bootstrap()
---
+++ backend/src/modules/database/prisma.service.ts
@@ -0,0 +1,20 @@
+import { environment } from '@/common/config/environment'
+import { PrismaClient } from '@generated/prisma'
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
+import { PrismaPg } from '@prisma/adapter-pg'
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const adapter = new PrismaPg({ connectionString: environment.DATABASE_URL })
+    super({ adapter })
+  }
+
+  async onModuleInit() {
+    await this.$connect()
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
+  }
+}
