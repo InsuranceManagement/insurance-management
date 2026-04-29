d5ecc518709792c987c37b42f55bf30e2e52f27c
Lucas Castelano <lucas.castelano27@gmail.com>
2026-04-29 13:35:49 -0300

    config: product type seed

--- backend/prisma/seed-data.ts
+++ backend/prisma/seed-data.ts
@@ -1,9 +1,46 @@
 export const seedData = {
   productType: [
-    { id: 'VIDA', name: 'Vida', description: 'Seguro de vida' },
-    { id: 'AUTO', name: 'Auto', description: 'Seguro automotivo' },
-    { id: 'RESIDENCIAL', name: 'Residencial', description: 'Seguro residencial' },
-    { id: 'SAUDE', name: 'Saúde', description: 'Seguro saúde' },
+    {
+      id: 'VIDA_QUALQUER_CAUSA',
+      name: 'Vida Qualquer Causa',
+      description: 'Seguro de vida por qualquer causa',
+    },
+    { id: 'MORTE_ACIDENTAL', name: 'Morte Acidental', description: 'Seguro por morte acidental' },
+    {
+      id: 'INVALIDEZ_PERMANENTE_ACIDENTE',
+      name: 'Invalidez Permanente por Acidente',
+      description: 'Seguro de invalidez permanente por acidente',
+    },
+    {
+      id: 'IPA_MAJORADA',
+      name: 'IPA Majorada',
+      description: 'Invalidez permanente por acidente majorada',
+    },
+    {
+      id: 'DIARIA_INCAPACIDADE_TEMPORARIA',
+      name: 'Diária por Incapacidade Temporária',
+      description: 'Seguro de diária por incapacidade temporária',
+    },
+    { id: 'DOENCAS_GRAVES', name: 'Doenças Graves', description: 'Seguro para doenças graves' },
+    { id: 'CIRURGIAS', name: 'Cirurgias', description: 'Seguro para cirurgias' },
+    { id: 'DIH_UTI', name: 'DIH e UTI', description: 'Diária de internação hospitalar e UTI' },
+    {
+      id: 'SAF_INDIVIDUAL',
+      name: 'SAF Individual',
+      description: 'Seguro de acidentes familiares individual',
+    },
+    { id: 'SAF_FAMILIAR', name: 'SAF Familiar', description: 'Seguro de acidentes familiares' },
+    {
+      id: 'RESPONSABILIDADE_CIVIL',
+      name: 'Responsabilidade Civil',
+      description: 'Seguro de responsabilidade civil',
+    },
+    {
+      id: 'SEGURANCA_EXCLUSIVA',
+      name: 'Segurança Exclusiva',
+      description: 'Seguro de segurança exclusiva',
+    },
+    { id: 'EMPRESARIAL', name: 'Empresarial', description: 'Seguro empresarial' },
   ],
   // TODO: Add insurance companies and products seeds
   insuranceCompany: [],
