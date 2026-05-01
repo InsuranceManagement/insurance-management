444ef735118a4fc15287c45749567e075b17c42e
Gabriel <gsmainiere1901@gmail.com>
2026-05-01 15:16:13 -0300

    feat: creating user entity

---
+++ backend/src/modules/user/entities/user.ts
@@ -0,0 +1,15 @@
+export class User {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly email: string,
+    public readonly password: string,
+    public readonly createdAt: Date,
+    public readonly updatedAt: Date,
+    public readonly deletedAt: Date | null,
+  ) {}
+
+  isActive(): boolean {
+    return this.deletedAt === null
+  }
+}
