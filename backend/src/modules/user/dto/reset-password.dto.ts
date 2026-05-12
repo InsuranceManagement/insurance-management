d113ea1152b9a7bd40aa3dcb4e0bdbe739ff18db
Gabriel <gsmainiere1901@gmail.com>
2026-05-12 19:50:23 -0300

    feat: updating reset dto

--- backend/src/modules/user/dto/reset-password.dto.ts
+++ backend/src/modules/user/dto/reset-password.dto.ts
@@ -1,12 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsString } from 'class-validator'
+import { IsString, Matches, MinLength } from 'class-validator'
 import { ResetPasswordInput } from '../inputs/reset-password.input'
 
 export class ResetPasswordDto implements ResetPasswordInput {
   @ApiProperty({ description: 'Token' })
+  @IsString()
   token!: string
 
   @ApiProperty({ description: 'Password' })
   @IsString()
+  @MinLength(8)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/, {
+    message:
+      'password must contain at least one uppercase letter, one lowercase letter and one number',
+  })
   password!: string
 }
