251cc88893f1a782592aedaae7ad92235a51767c
Lucas Coqui <160676396+Coqui000@users.noreply.github.com>
2026-05-18 13:21:45 -0300

    Feat/product type crud (#39)
    * feat: Create product-type inputs and dto
    
    * feat: Create product-type repository
    
    * feat: Create product-type service
    
    * feat: Create product-type controller and module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,23 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { PrismaModule } from './modules/database/prisma.module'
 import { ClientModule } from './modules/client/client.module'
+import { PrismaModule } from './modules/database/prisma.module'
 import { InsuranceCompanyModule } from './modules/insurance-company/insurance-company.module'
+import { ProductTypeModule } from './modules/product-type/product-type.module'
 import { ProductModule } from './modules/product/product.module'
 import { UserModule } from './modules/user/user.module'
 
 @Module({
-  imports: [PrismaModule, UserModule, InsuranceCompanyModule, ProductModule, ClientModule],
+  imports: [
+    PrismaModule,
+    UserModule,
+    InsuranceCompanyModule,
+    ProductModule,
+    ClientModule,
+    ProductTypeModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
