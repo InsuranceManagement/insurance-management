a87f598fa51f07a29d3d556fe54bfd62a1719e39
Gabriel <gsmainiere1901@gmail.com>
2026-05-18 12:51:55 -0300

    feat: updating app.module e main.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common'
+import { APP_GUARD } from '@nestjs/core'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { AuthGuard } from './common/auth/auth.guard'
+import { AuthModule } from './common/auth/auth.module'
 import { PrismaModule } from './modules/database/prisma.module'
 import { ClientModule } from './modules/client/client.module'
 import { InsuranceCompanyModule } from './modules/insurance-company/insurance-company.module'
@@ -8,8 +11,8 @@ import { ProductModule } from './modules/product/product.module'
 import { UserModule } from './modules/user/user.module'
 
 @Module({
-  imports: [PrismaModule, UserModule, InsuranceCompanyModule, ProductModule, ClientModule],
+  imports: [PrismaModule, AuthModule, UserModule, InsuranceCompanyModule, ProductModule, ClientModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: APP_GUARD, useClass: AuthGuard }],
 })
 export class AppModule {}
