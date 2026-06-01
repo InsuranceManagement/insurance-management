a09f1d73a45e293c81a6d1d908140f92242592ee
Lucas Castelano <lucas.castelano27@gmail.com>
2026-06-01 13:49:33 -0300

    chore: remove wrongly added types

--- frontend/src/shared/models/charts/chart-point.ts
+++ frontend/src/shared/models/charts/chart-point.ts
@@ -6,20 +6,3 @@ export interface ChartPoint {
   y: number
   color?: string
 }
-
-export type ChartSeriesType =
-  | "line"
-  | "spline"
-  | "area"
-  | "column"
-  | "bar"
-  | "pie"
-  | "scatter"
-  | "kpi"
-
-export interface ChartSeries {
-  name: string
-  type: ChartSeriesType
-  data: ChartPoint[]
-  unit?: string
-}
