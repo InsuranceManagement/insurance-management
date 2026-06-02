d703ca231bfa1b23f86310f8b8dc691aaa8b1599
Gabriel <gsmainiere1901@gmail.com>
2026-06-01 23:15:11 -0300

    feat: implementing register schema + model

--- frontend/src/features/Auth/models/auth.ts
+++ frontend/src/features/Auth/models/auth.ts
@@ -12,3 +12,9 @@ export interface AuthResponse {
     email: string
   }
 }
+
+export interface RegisterRequest {
+  name: string
+  email: string
+  password: string
+}
--- frontend/src/features/Auth/schema.ts
+++ frontend/src/features/Auth/schema.ts
@@ -5,4 +5,11 @@ export const loginSchema = z.object({
   password: z.string().min(6),
 })
 
+export const registerSchema = z.object({
+  name: z.string().min(2),
+  email: z.email(),
+  password: z.string().min(6),
+})
+
 export type LoginFormValues = z.infer<typeof loginSchema>
+export type RegisterFormValues = z.infer<typeof registerSchema>
