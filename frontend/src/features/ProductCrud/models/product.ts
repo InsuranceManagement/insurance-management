cb2428bf0e99895e2f40cf622982b053cc55a095
BelmontRafael <rafaelmagalhaes12323@gmail.com>
2026-06-04 19:23:25 -0300

    feat: add product model

---
+++ frontend/src/features/ProductCrud/models/product.ts
@@ -0,0 +1,17 @@
+import { type EntityWithName } from "@/shared/models/entity"
+import { type InsuranceCompany } from "@/features/InsuranceCompanyCrud/models/insurance-company"
+import { type ProductType } from "@/features/ProductTypeCrud/models/product-type"
+
+export interface Product extends EntityWithName {
+  productTypeId: string
+  insuranceCompanyId: string
+  // Assumindo que a rota GET /products do backend popule essas relações.
+  // Se não popular, a tabela usará apenas os IDs.
+  productType?: ProductType
+  insuranceCompany?: InsuranceCompany
+}
+
+export type ProductUpsertPayload = Pick<
+  Product,
+  "name" | "productTypeId" | "insuranceCompanyId"
+>
