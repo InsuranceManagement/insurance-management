abe012182778ef4d187a327db65d3c858dca32ed
Gabriel <gsmainiere1901@gmail.com>
2026-06-05 21:27:31 -0300

    feat: updating schema.ts

--- frontend/src/features/Auth/schema.ts
+++ frontend/src/features/Auth/schema.ts
@@ -15,6 +15,20 @@ export const forgotPasswordSchema = z.object({
   email: z.email(),
 })
 
+export const resetPasswordSchema = z
+  .object({
+    password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres"),
+
+    confirmPassword: z
+      .string()
+      .min(6, "A confirmação deve ter pelo menos 6 caracteres"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "As senhas devem ser iguais",
+  })
+
 export type LoginFormValues = z.infer<typeof loginSchema>
 export type RegisterFormValues = z.infer<typeof registerSchema>
 export type ForgotPasswordFormValues = z.infer<typeof forgotPasswordSchema>
+export type ResetPasswordFormValues = z.infer<typeof resetPasswordSchema>
